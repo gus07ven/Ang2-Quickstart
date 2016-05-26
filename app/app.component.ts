80892ea2c00a60e07e8d091db540fbce7574633a
gus07ven <gus07ven@users.noreply.github.com>
2016-05-25 21:44:42 -0500

    Upload quickstart app files

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `'<h1>My First Angular 2 App</h1>'
+    <h2> I'm a new application and I challenge Alejandro to say:
+     "Tres tristes tigres comen trigo en un trigal."</h2>
+`
+})
+
+export class AppComponent
+{
+
+}
